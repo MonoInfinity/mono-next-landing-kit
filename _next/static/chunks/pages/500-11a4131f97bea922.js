2ba0714f4dd3f0d8de29fbcd95e6e88bdcfef0f8
Heaty566 <heaty566@gmail.com>
2022-10-09 13:59:18 +0000

    Deploying to gh-pages from  @ 104aaaa3d033a8c3faa1fe05cfc65fe59b042890 🚀

---
+++ _next/static/chunks/pages/500-11a4131f97bea922.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[573],{698:function(a,b,c){(window.__NEXT_P=window.__NEXT_P||[]).push(["/500",function(){return c(3897)}])},3897:function(a,b,c){"use strict";c.r(b);var d=c(5893);c(7294);var e=function(){return(0,d.jsx)("div",{children:"123"})};b.default=e}},function(a){a.O(0,[774,888,179],function(){var b;return a(a.s=698)}),_N_E=a.O()}])
