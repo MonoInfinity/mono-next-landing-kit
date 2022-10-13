c80502ab822d9f5dbc8078122010d685d38c8ade
vinhnhan <heaty566@gmail.com>
2022-10-13 21:02:10 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,31 @@ import { useWindowDimensions } from '../src/core/hooks/useWindowDimension';
 interface HomePageProps {}
 
 const content = [
-    { name: 'Name 1', categories: ['NextJS', 'Micro Service', 'TailwindCSS', 'Firebase', 'NestJS'] },
-    { name: 'Name 2', categories: ['cate1', 'cate2', 'cate3', 'cate4', 'cate 5'] },
-    { name: 'Name 3', categories: ['cate1', 'cate2', 'cate3', 'cate4', 'cate 5'] },
-    { name: 'Name 4', categories: ['cate1', 'cate2', 'cate3', 'cate4', 'cate 5'] },
-    { name: 'Name 5', categories: ['cate1', 'cate2', 'cate3', 'cate4', 'cate 5'] },
+    {
+        name: 'Cyber One',
+        imageUrl: 'https://i.pinimg.com/originals/aa/e2/2d/aae22da725d8375d83859c70ce41494c.jpg',
+        categories: ['NextJS', 'Micro Service'],
+    },
+    {
+        name: 'Equo VN',
+        imageUrl: 'https://m.media-amazon.com/images/I/71hjZzOIIQS._AC_SL1500_.jpg',
+        categories: ['cate1', 'cate2', 'cate3'],
+    },
+    {
+        name: 'The Hood',
+        imageUrl: 'https://c4.wallpaperflare.com/wallpaper/820/473/281/1980s-car-delorean-neon-wallpaper-preview.jpg',
+        categories: ['cate1', 'cate2', 'cate3'],
+    },
+    {
+        name: 'Cyber Core',
+        imageUrl: 'https://i.pinimg.com/originals/48/ba/48/48ba48f560f01c114d3459ecc0cdbb5a.jpg',
+        categories: ['cate1', 'cate2', 'cate3'],
+    },
+    {
+        name: 'Trip 2',
+        imageUrl: 'https://www.topgear.com/sites/default/files/news-listicle/image/2020/10/2_-_chevillon_thrax.jpg?w=827&h=465',
+        categories: ['cate1', 'cate2', 'cate3'],
+    },
 ];
 
 const HomePage: React.FC<HomePageProps> = () => {
@@ -34,11 +54,11 @@ const HomePage: React.FC<HomePageProps> = () => {
 
     return (
         <ContainerLayout>
-            <div className="relative min-h-screen flex w-full text-white flex-col items-center space-y-12 lg:space-y-20 md:space-y-16 sm:space-y-14">
+            <div className="relative flex flex-col items-center w-full min-h-screen space-y-12 text-white lg:space-y-20 md:space-y-16 sm:space-y-14">
                 <div className="flex flex-col items-center">
-                    <div className="flex items-center sm:space-x-2 space-x-0 justify-center">
-                        <div className="relative h-28 w-28  rounded-lg rotate-12">
-                            <div className="w-full rotate-0 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute">
+                    <div className="flex flex-col items-center justify-center space-y-4 ">
+                        <div className="relative w-20 h-20 rounded-lg rotate-12">
+                            <div className="absolute w-full rotate-0 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
                                 <img alt="" src="/assets/gif/logo-gif.gif" className="object-center" />
                             </div>
                             <div className="w-full rotate-[120deg] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute">
@@ -48,8 +68,10 @@ const HomePage: React.FC<HomePageProps> = () => {
                                 <img alt="" src="/assets/gif/logo-gif.gif" className="object-center" />
                             </div>
                         </div>
-
-                        <h1 className="font-semibold md:text-8xl sm:text-6xl text-4xl ">Mono Infinity</h1>
+                        <div className="flex flex-col items-center justify-center px-4 space-y-1 text-xl font-bold tracking-wider hello">
+                            <h1 className="text-lg lg:text-4xl">Building Your Modern Web App</h1>
+                            <h1 className="space-y-4 md:text-5xl ">Being the higher level</h1>
+                        </div>
                     </div>
                 </div>
 
@@ -74,14 +96,18 @@ const HomePage: React.FC<HomePageProps> = () => {
                         {content.map((item, index) => (
                             <SwiperSlide key={`${item.name}-${index}`}>
                                 <div className="min-w-[280px] h-[480px] w-full flex flex-col rounded-2xl overflow-hidden border-violet-600 border ">
-                                    <div className="bg-gray-400 h-full"></div>
-                                    <div className="bg-violet-900 py-6 flex items-start px-4 space-x-2 ">
-                                        <div className="bg-gray-900 h-12 w-12 rounded-full flex-shrink-0"></div>
-                                        <div className="flex flex-col space-y-1">
+                                    <div className="bg-gray-400 h-80">
+                                        <img src={item.imageUrl} className="object-cover w-full h-full" />
+                                    </div>
+                                    <div className="flex items-start flex-1 px-4 py-4 space-x-2 bg-violet-900 ">
+                                        {/* <div className="flex-shrink-0 w-12 h-12 bg-gray-900 rounded-full"></div> */}
+                                        <div className="flex flex-col space-y-2">
                                             <p>{item.name}</p>
-                                            <div className="flex space-x-2 flex-wrap justify-start items-start">
+                                            <div className="flex flex-wrap items-start justify-start space-x-2">
                                                 {item.categories.map((cate, index) => (
-                                                    <div className="text-sm p-1 border-2 mb-2 rounded-2xl w-fit">{cate}</div>
+                                                    <div key={index} className="px-2 py-1 mb-2 text-xs border-2 rounded-2xl w-fit">
+                                                        {cate}
+                                                    </div>
                                                 ))}
                                             </div>
                                         </div>
--- src/core/components/layouts/BackgroundLayout.tsx
+++ src/core/components/layouts/BackgroundLayout.tsx
@@ -4,13 +4,13 @@ interface BackgroundLayoutProps extends React.PropsWithChildren {}
 
 const BackgroundLayout: React.FunctionComponent<BackgroundLayoutProps> = ({ children }) => {
     return (
-        <div className="relative h-full w-full">
-            <div className="bg-black top-0 left-0 h-full w-full absolute">
-                <div className="bg-blue-800/20 h-full w-full ">
-                    <div className="bg-gradient-to-r from-indigo-500/20 via-purple-500/20 to-pink-500/20 h-full relative blur-3xl -z-"></div>
+        <div className="relative w-full h-full">
+            <div className="absolute top-0 left-0 w-full h-full bg-black">
+                <div className="w-full h-full bg-blue-800/20 ">
+                    <div className="relative h-full bg-gradient-to-r from-indigo-500/20 via-purple-500/20 to-pink-500/20 blur-3xl -z-"></div>
                 </div>
             </div>
-            <div className="pt-40">{children}</div>
+            <div className="pt-24">{children}</div>
         </div>
     );
 };
