90e0db97ecc04afee3a23305cbc7f157c1a10527
Vĩnh Nhân  Phạm <nhan.pham@solidbytes.vn>
2022-12-10 22:06:40 +0700

    update

--- src/core/components/commons/CommonSeo.tsx
+++ src/core/components/commons/CommonSeo.tsx
@@ -6,5 +6,25 @@ interface CommonSeoProps {
 }
 
 export const CommonSeo: React.FC<CommonSeoProps> = ({ title }) => {
-    return <NextSeo title={`${title} `} />;
+    return (
+        <NextSeo
+            title={title}
+            description="Mono Infinity is software development company that provides software development services, web development, mobile app development, and digital marketing services"
+            openGraph={{
+                title,
+                url: 'https://monoinfinity.net',
+                description:
+                    'Mono Infinity is software development company that provides software development services, web development, mobile app development, and digital marketing services',
+                images: [
+                    {
+                        url: '/assets/images/banner.png',
+                        width: 800,
+                        height: 600,
+                        alt: `${title}`,
+                        type: 'image/png',
+                    },
+                ],
+            }}
+        />
+    );
 };
